49e52ab03d931a308287b441a4edfa2af6ed48de
AleksejsLoginovs <aloginovs@dundee.ac.uk>
2017-07-25 17:44:38 +0100

    more learning

--- TestAddOn.lua
+++ TestAddOn.lua
@@ -19,7 +19,7 @@ local function print_character_info(char_table)
 	print("Level: " .. char_table.level)
 	print("Health: " .. char_table.health)
 	print("Class: " .. char_table["class"])
-	print("Size of the table (no indexed elements in a table):" .. #char_table)
+	print("Size of the table (4 indexed elements in a table):" .. #char_table)
 end
 
 local tadn_tbl = {1,2,3,4,5,6,7,8,9,10} --first element is at location 1 instead of 0
@@ -27,6 +27,7 @@ tadn_tbl["key_of_element"] = 15
 
 --creating a table of values
 local character_table = {
+1,2,3,4, --just random values for one of the examples
 	["name"] = UnitName("player"),
 	health = UnitHealth("player"),
 	["class"] = UnitClass("player"),
@@ -50,9 +51,8 @@ local stack_class = {
 			return rtn
 		end
 	end,
-
-
 }
+
 --using functions of a class
 stack_class.push(stack_class, 10)--need to pass it itself as one of the arguments is 'self' ONLY IF USING . accessor
 print(stack_class[3])
@@ -62,3 +62,16 @@ print(stack_class:pop(6))--if : accessor is used it generates the 'self' argumen
 greeting(tadn_player_name, tadn_player_level, tadn_player_hp)
 count_number_of_elements(1,2,3,4,5,6,7,"asd", "twentyfive",{1,"name"})
 print_character_info(character_table)
+
+
+--loop
+local endValue = 10
+print("Counting loop:")
+for i = 1, endValue, 1 do --when i is GREATER than endValue it BREAKS
+	print(i)
+end
+
+print("Character table loop: ")
+for key, value in pairs(character_table) do --can put ipairs instead of pairs to print only indexed part of table
+	print(key .. " : " .. value)
+end
